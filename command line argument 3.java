1c5acf2a48667fc6b0c85080dface889ffea51a1
Swathi201801150 <66115090+Swathi201801150@users.noreply.github.com>
2020-05-30 01:15:08 +0530

    Add files via upload

---
+++ command line argument 3.java
@@ -0,0 +1,12 @@
+
+
+public class Main
+{
+	public static void main(String[] args){
+	    int x,y,s;
+	    x=Integer.parseInt(args[0]);
+	    y=Integer.parseInt(args[1]);
+	    s=x+y;
+		System.out.println("The sum of " + x + " and " + y +" is " +s);
+	}
+}
